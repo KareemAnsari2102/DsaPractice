bf9f379addb63d01cb7328223836600139617c88
KareemAnsari2102 <ansarikareemwork@gmail.com>
2023-11-16 16:47:58 +0530

    "twosum commit"

---
+++ groupAnagrams.py
@@ -0,0 +1,9 @@
+"""
+Given an array of strings strs, group the anagrams together. you can return the answer in any order. 
+"""
+
+def groupAnagrams(strs: list[str]) -> list[list[str]]:
+    
+    print("hello")
+    
+    groupAnagrams(["Hello", "like", "and", "subscribe"])
--- twoSum.py
+++ twoSum.py
@@ -2,7 +2,15 @@
  given an array of integers nums and an integer target, return indices of the two numbers 
  such that they add up to target.
 """
-def twosum():
-    print("Hello")
+def twosum(nums: list[int], target: int) -> [int]:
+    map = {}
     
-twosum()
+    for i, n in enumerate(nums):
+        diff = target - n
+        if diff in map:
+            print(map[diff], i)
+            return[map[diff], i]
+        map[n] = i
+    return
+        
+twosum([2, 7, 11, 15], 9)
