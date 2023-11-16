e625ae254c81d5f34759e94151318603dc6fc7cc
KareemAnsari2102 <ansarikareemwork@gmail.com>
2023-11-16 19:06:26 +0530

    "Top k frequent question"

---
+++ topKfrequentElements.py
@@ -0,0 +1,20 @@
+def topKfrequent(nums: list[int], k: int) -> list[int]:
+    
+    count = {}
+    freq = [[] for i in range(len(nums) + 1)]
+    
+    for n in nums:
+        count[n] = 1 + count.get(n, 0)
+    for n, c in count.items():
+        freq[c].append(n)
+        
+    res = []
+    for i in range(len(freq) - 1, 0, -1):
+        for n in freq[i]:
+            res.append(n)
+            if len(res) == k:
+                print(res)
+                return res
+    
+    
+topKfrequent([1,2,7,8,3,4,5,6,7,8,3], 4)
