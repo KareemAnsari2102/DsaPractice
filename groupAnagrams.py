eb1d422d6cbe0655ff3921e04f4ef9308db8f5ef
KareemAnsari2102 <ansarikareemwork@gmail.com>
2023-11-16 17:36:48 +0530

    "groupAnagrams problem"

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -1,9 +1,20 @@
 """
 Given an array of strings strs, group the anagrams together. you can return the answer in any order. 
 """
+from collections import defaultdict
 
 def groupAnagrams(strs: list[str]) -> list[list[str]]:
     
-    print("hello")
+    res = defaultdict(list)
     
-    groupAnagrams(["Hello", "like", "and", "subscribe"])
+    for s in strs:
+        count = [0] * 26
+        
+        for c in s:
+            count[ord(c)- ord("a")] += 1
+            
+        res[tuple(count)].append(s)
+    print(res.values())
+    return res.values()
+
+groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
